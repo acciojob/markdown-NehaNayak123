542bbdea22ceb028b82a01d2c05229dd79fd8921
Neha Nayak <nayakneha951@gmail.com>
2025-04-05 01:45:08 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,13 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+// App.js
+import React from "react";
+import MarkdownEditor from "./MarkdownEditor";
 
+function App() {
+  return (
+    <div className="app">
+      <MarkdownEditor />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MarkdownEditor.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { marked } from "marked";
+
+function MarkdownEditor() {
+  const [markdown, setMarkdown] = useState("# Hello, Markdown!\nType your text here...");
+  const [html, setHtml] = useState("");
+
+  useEffect(() => {
+    setHtml(marked.parse(markdown));
+  }, [markdown]);
+
+  return (
+    <div className="editor-container">
+      <textarea
+        className="textarea"
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
+      />
+      <div
+        className="preview"
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
+    </div>
+  );
+}
+
+export default MarkdownEditor;
